0f50524a966682dd6cce4703ffcbaf93127f1e56
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-03-30 20:49:43 -0400

    delete

--- configs/passport/passport.config.js
+++
@@ -1,9 +0,0 @@
-const passport = require('passport');
-
-require('./local-strategy.config');
-require('./serializers.config');
-
-module.exports = app => {
-  app.use(passport.initialize()); // this "fires" the passport package
-  app.use(passport.session()); // connects passport to sessions
-};
