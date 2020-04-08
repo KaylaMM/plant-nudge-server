283b7626036afebfb889bfd4fa860781966f0e01
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-04-08 00:16:09 -0400

    finished plant route, full CRUD

--- routes/plant.js
+++ routes/plant.js
@@ -29,15 +29,40 @@ router.post("/plant", (req, res, next) => {
 
   newPlant
     .save()
-    .then((plant => {})
+    .then((plant => {
+      res.render(plant)
+    })
     .catch((error) => {
       message: "Something went wrong";
     });
 });
 
 //Read (Plant) Documents
+router.get('/plant', (req, res, next) => {
+  Plant.find(allUserPlantsFromDB)
+  .then(allUserPlantsFromDB => {
+    console.log('Retrieved books from DB:', allUserPlantsFromDB);
+  })
+  .catch(error => {
+    console.log('Error while getting the plants from the DB: ', error);
+  })
+  res.render('plants')
+})
 
+router.get('/plant/:plantId', (req, res, next) => {
+  console.log('The ID from the URL is: ', plantId);
+  res.render('plants');
+});
 
+router.get('/plant/:plantId', (req, res, next) => {
+  Book.findOne({'_id': req.params.bookId})
+    .then(thePlant => {
+      res.render('plants', { plant: thePlant });
+    })
+    .catch(error => {
+      console.log('Error while retrieving plant details: ', error);
+    })
+});
 
 
 //Update a Plant
@@ -77,6 +102,14 @@ router.post('/plant', (req, res, next) => {
 
 //Delete Plant
 router.post('/plant', (req, res, next) => {
-  Plant.delete({_id: req.params.plant_id})
-  .then((deletedPlant))
+  Plant.delete({_id: req.params.plant_id}, {
+    $pull: {plants: req.params.plant_id}
+  })
+  .then((deletedPlant) => {
+    res.redirect('plants')
+
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 })
